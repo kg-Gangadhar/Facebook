4a44b41547607bfb0868d203593f3e317e80b6bc
kg-Gangadhar <kondagangadhar184@gmail.com>
2022-09-15 23:14:14 +0530

    updated

--- DButilites.sh
+++ DButilites.sh
@@ -1,4 +1,4 @@
-hello this line is updatewd in dev branch
+hello this line is updated in dev branch
 hi these is a file for dbtlites.java
 DButilites added..
 hello this is the new line in master branch.
